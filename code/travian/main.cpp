90f8bb31afc60886b3a68fca7b42e1ee55fc0ec8
rost <rostepifanov@gmail.com>
2019-02-18 22:49:00 +0700

    добавленно dom дерево для разбора html

--- code/travian/main.cpp
+++ code/travian/main.cpp
@@ -1,9 +1,10 @@
 #include "app.h"
 #include <curl/curl.h>
 
+
 int main(void)
 {
-    player p(defs::keys("https://ts19.travian.ru/", "repifanov","12345678"));
+    player p(defs::keys("https://ts4.travian.ru/", "repifanov","12345678"));
 
     app::get_instance().run(p);
 }
